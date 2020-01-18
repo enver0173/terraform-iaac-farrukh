614e92de63df8b83bbea195590b2f9314adbff7b
enver0173 <--list>
2020-01-18 12:59:56 -0600

    adeded access

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -4,7 +4,6 @@ terraform {
     storage_account_name = "enver0173"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    subscription_id      = "56f772b8-c7bc-458e-b76d-ca00f5f09a29"
-    tenant_id            = "9a75275d-85f7-4e9f-95f8-e226a316ed97"
+    access_key           = ""
   }
 }
